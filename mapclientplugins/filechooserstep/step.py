9859e4c312e288686b97a5ea67d7d49cbd9c6e96
TimmyNic <tim.nicolas@outlook.com>
2021-06-09 11:56:13 +1200

    Updated ConfigureDialog() arguments.

--- mapclientplugins/filechooserstep/step.py
+++ mapclientplugins/filechooserstep/step.py
@@ -59,7 +59,7 @@ class FileChooserStep(WorkflowStepMountPoint):
         then set:
             self._configured = True
         '''
-        dlg = ConfigureDialog()
+        dlg = ConfigureDialog(self._main_window)
         dlg.identifierOccursCount = self._identifierOccursCount
         dlg.setConfig(self._config)
         dlg.validate()
