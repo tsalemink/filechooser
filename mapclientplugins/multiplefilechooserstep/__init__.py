d57802cc25a78f0f24a67fbdb4ad4e682a8355c7
TimmyNic <tim.nicolas@outlook.com>
2022-12-20 13:14:10 +1300

    Set version to 0.1.3

--- mapclientplugins/multiplefilechooserstep/__init__.py
+++ mapclientplugins/multiplefilechooserstep/__init__.py
@@ -3,7 +3,7 @@
 MAP Client Plugin
 """
 
-__version__ = '0.1.2'
+__version__ = '0.1.3'
 __author__ = 'Hugh Sorby'
 __stepname__ = 'Multiple File Chooser'
 __location__ = 'https://github.com/mapclient-plugins/multiplefilechooser'
